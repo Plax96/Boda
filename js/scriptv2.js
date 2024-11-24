da0cac7497b3774e7028c7b3550c87b858b4a6fa
Plax96 <placido.castellanos@gmail.com>
2024-11-24 12:21:47 -0600

    Initial commit

---
+++ js/scriptv2.js
@@ -0,0 +1,4 @@
+// Agrega funcionalidad al botón
+document.getElementById("miBoton").addEventListener("click", function() {
+    document.getElementById("mensaje").innerText = "¡Has hecho clic en el botón!";
+});
